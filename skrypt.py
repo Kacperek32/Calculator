62577f81dc91afe75d15bcac94e775bc9ba91506
Kacper Manterys <kacpermanterys@gmail.com>
2022-06-02 19:08:24 +0200

    Add if in

--- skrypt.py
+++ skrypt.py
@@ -9,4 +9,9 @@ def multi(number, number3):
     return number * number3
 def div(number, number3):
     return number / number3
+
 calculating = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+
+if calculating in ("1", "2", "3", "4"):
+    number1 = float(input("Podaj składnik 1: "))
+    number2 = float(input("Podaj składnik 2: "))
